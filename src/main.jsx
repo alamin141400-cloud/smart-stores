6f22bd3fe3e7b9ffe25083da594888ec27587ecc
alamin141400-cloud <alamin141400@gmail.com>
2026-03-26 18:46:36 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SmartStores from './SmartStores'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SmartStores />
+  </React.StrictMode>
+)
